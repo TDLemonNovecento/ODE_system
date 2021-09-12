e8f9a36077c85ac02e8a79c85cd038e6ede75ff0
TDLemonNovecento <lemon.novecento@bluewin.ch>
2021-09-12 16:09:23 +0200

    added local variables

---
+++ var.py
@@ -0,0 +1,14 @@
+# M.Stuke, 27.08.2021
+
+# stores private path variables
+# Here:example files of eye system
+
+import os
+
+path = os.getcwd() + "/EYE/"
+
+sbml_filename = path + "eye_ode.sbml"
+m_figuremaker = path + "createfigure.m"
+
+sbml_cleanfile = path + "cleanfile.sbml"
+plotfile = path + "plotfile.py"
